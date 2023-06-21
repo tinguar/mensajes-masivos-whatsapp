7b8bcf0b8463ea8ca3a361a6c75a70d74ab20ffb
SciedtecInfo <134951396+SciedtecInfo@users.noreply.github.com>
2023-06-21 21:36:04 +0000

    Pending changes exported from your codespace

---
+++ backend/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import pandas as pd
+
+app = Flask(__name__)
+CORS(app)  # Habilitar CORS en la aplicación Flask
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    if file:
+        filename = file.filename
+        if filename.endswith('.csv'):
+            df = pd.read_csv(file)
+        elif filename.endswith('.xml'):
+            df = pd.read_xml(file)
+        else:
+            return jsonify({'error': 'Invalid file format'})
+        
+        # Aquí puedes realizar cualquier procesamiento o manipulación de los datos
+        
+        return jsonify({'message': 'File uploaded successfully'})
+    else:
+        return jsonify({'error': 'No file uploaded'})
+
+if __name__ == '__main__':
+    app.run()
--- w_send/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WSendConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'w_send'
